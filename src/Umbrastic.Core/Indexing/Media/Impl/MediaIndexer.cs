8c17aee9cc2f4f7adbab1998cc58d6b1b490c95b
neilha03 <neilha03@NSS010906>
2017-06-02 21:36:30 +0100

    added mediaindexer class

---
+++ src/Umbrastic.Core/Indexing/Media/Impl/MediaIndexer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Umbraco.Core.Logging;
+using Umbrastic.Core.Utils;
+
+namespace Umbrastic.Core.Indexing.Media.Impl
+{
+    public class MediaIndexer : IEntityIndexer
+    {
+        public void Build(string indexName)
+        {
+            using (BusyStateManager.Start($"Building media for {indexName}", indexName))
+            {
+                LogHelper.Info<MediaIndexer>($"Started building index [{indexName}]");
+                foreach (var indexService in UmbracoSearchFactory.GetMediaIndexServices())
+                {
+                    try
+                    {
+                        LogHelper.Info<MediaIndexer>($"Started to index media for {indexService.DocumentTypeName}");
+                        BusyStateManager.UpdateMessage($"Indexing {indexService.DocumentTypeName}");
+                        indexService.Build(indexName);
+                    }
+                    catch (Exception ex)
+                    {
+                        LogHelper.Error<MediaIndexer>($"Failed to index media for {indexService.DocumentTypeName}", ex);
+                    }
+                }
+                LogHelper.Info<MediaIndexer>(
+                    $"Finished building index [{indexName}] : elapsed {BusyStateManager.Elapsed.ToString("g")}");
+            }
+        }
+    }
+}
