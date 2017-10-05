5cea4190983f7ae7ffe7c733385379ad508e9d11
neilha03 <neilha03@NSS010906>
2017-10-05 17:57:27 +0100

    fixed first publish URL bug and unpublished changes bug

--- src/Umbrastic.Core/Indexing/Content/Impl/ContentIndexService.cs
+++ src/Umbrastic.Core/Indexing/Content/Impl/ContentIndexService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Umbraco.Core;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
 using Umbraco.Web;
@@ -35,12 +36,31 @@ namespace Umbrastic.Core.Indexing.Content.Impl
         protected override sealed IEnumerable<IContent> RetrieveIndexItems(ServiceContext serviceContext)
         {
             var contentType = serviceContext.ContentTypeService.GetContentType(DocumentTypeName);
-            return serviceContext.ContentService.GetContentOfContentType(contentType.Id).Where(x => x.Published);
+
+            // get the published version of each indexable content node of this type
+            return serviceContext.ContentService.GetContentOfContentType(contentType.Id)
+                .Where(x => x.HasPublishedVersion)
+                .Select(x => x.Published ? x : serviceContext.ContentService.GetPublishedVersion(x));
         }
 
         public override sealed bool ShouldIndex(IContent entity)
         {
             return entity.ContentType.Alias.Equals(IndexTypeName, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        protected override string UrlFor(IContent contentInstance)
+        {
+            var url = base.UrlFor(contentInstance);
+
+            // on first publish the url will be # so manually build it
+            if (url == "#")
+            {
+                var helper = new UmbracoHelper(UmbracoContext.Current);
+                var parent = helper.TypedContent(contentInstance.ParentId);
+                url = parent != null ? $"{parent.Url}{contentInstance.Name.ToUrlSegment()}/" : url;
+            }
+
+            return url;
         }
     }
 }
